65105b687300ab947968171288d4837e43ec5737
Shine_Star <rlagksquf30@gmail.com>
2017-11-30 04:03:43 +0900

    output text

--- programming_project_2/main.cpp
+++ programming_project_2/main.cpp
@@ -4,15 +4,15 @@
 #include <string>
 #include <sstream>
 
-inline void input1();
-inline void input2();
-inline void input3();
 void knapsack(int* w, int** val, int n, int m, int* vals);
 int max(int a, int b);
 void BTknapsack(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n);
 bool Promising(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n);
 void BBknapsack(int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n);
 int bound(int level, int totalProfit, int totalWeight, int m, int* w, int* p, int n);
+inline void input1();
+inline void input2();
+inline void input3();
 
 int m, n;
 // 1.
@@ -33,22 +33,28 @@ int main() {
 	int maxProfit = 0;
 	ofs << "2017-2 Algorithms Programming Project 2, #21 Han-Byul Kim" << std::endl;
 	ofs << "The Detail execution process is on Console." << std::endl;
+	ofs << std::endl;
 
 	// 1. Dynamic Programming
 	input1();
 	std::cout << "1. Dynamic Programming(result table)" << std::endl;
+	ofs << "1. Dynamic Programming(result table)" << std::endl;
 	knapsack(weights_d, val_d, n, m, vals_d);
 	std::cout << std::endl;
+	ofs << std::endl;
 
 	// 2. Backtracking
 	input2();
 	std::cout << "2. Backtracking" << std::endl;
+	ofs << "2. Backtracking" << std::endl;
 	BTknapsack(btval, 0, 0, 0, maxProfit, m, weights, vals, n);
 	std::cout << std::endl;
+	ofs << std::endl;
 
 	// 3. Branch & Bound(Best First Search)
 	input3();
 	std::cout << "3. Branch & Bound" << std::endl;
+	ofs << "3. Branch & Bound" << std::endl;
 	maxProfit = 0;
 	BBknapsack(0, 0, 0, maxProfit, m, weights_t, vals_t, n);
 
@@ -65,6 +71,155 @@ int main() {
 	system("pause");
 
 	return 0;
+}
+
+
+void knapsack(int* w, int** val, int n, int m, int* vals) {
+	// The first line of table
+	for (int j = 0; j <= m; j++) // nothing contained
+		val[0][j] = 0;
+
+	// Calculating Values
+	for (int i = 1; i <= n; i++) { // start with 1st object
+		for (int j = 0; j <= w[i] - 1; j++)		// value before the weight
+			val[i][j] = val[i - 1][j];
+
+		for (int j = w[i]; j <= m; j++)		// value after the weight
+			val[i][j] = max(val[i - 1][j - w[i]] + vals[i], val[i - 1][j]);	// finding maximum between combinations
+	}
+
+	// Print Elements
+	int max = 0;
+	int max_m;
+	for (int i = 0; i <= n; i++) {
+		for (int j = 0; j <= m; j++) {
+			std::cout << val[i][j] << " ";
+			ofs << val[i][j] << " ";
+			if (val[i][j] > max) {
+				max = val[i][j];
+				max_m = j;
+			}
+		}
+		std::cout << std::endl;
+		ofs << std::endl;
+	}
+	std::cout << "-- result --" << std::endl;
+	std::cout << "totalProfit : " << max << std::endl;
+	std::cout << "totalWeight : " << max_m << std::endl;
+	ofs << "-- result --" << std::endl;
+	ofs << "totalProfit : " << max << std::endl;
+	ofs << "totalWeight : " << max_m << std::endl;
+}
+
+int max(int a, int b) {
+	if (a>b) return a;
+	else return b;
+}
+
+void BTknapsack(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {
+	if (totalWeight <= m && level == n) { // answer
+		std::cout << "Contained(1) or not(0)" << std::endl;
+		ofs << "Contained(1) or not(0)" << std::endl;
+		for (int i = 1; i <= n; i++) { std::cout << i << " | "; ofs << i << " | "; }
+		std::cout << std::endl;
+		ofs << std::endl;
+		for (int i = 1; i <= n; i++) { std::cout << btval[i] << " | "; ofs << btval[i] << " | "; }
+		std::cout << std::endl;
+		ofs << std::endl;
+	}
+
+	if (totalWeight <= m && totalProfit > maxProfit) maxProfit = totalProfit;	// updating max profit
+
+	if (Promising(btval, level, totalProfit, totalWeight, maxProfit, m, w, p, n)) {
+		// Left Child
+		btval[level + 1] = 1;
+		BTknapsack(btval, level + 1, totalProfit + p[level + 1], totalWeight + w[level + 1], maxProfit, m, w, p, n);
+		// Right Child
+		btval[level + 1] = 0;
+		BTknapsack(btval, level + 1, totalProfit, totalWeight, maxProfit, m, w, p, n);
+	}
+}
+
+bool Promising(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {		// Calculate promising
+	if (totalWeight >= m) return false;
+	int j = level + 1;
+	int upperBound = totalProfit;
+	int weightLimit = totalWeight;
+
+	while (j <= n && weightLimit + w[j] <= m) {	//calculating bounds before the limit of weight
+		weightLimit += w[j];
+		upperBound += p[j];
+		j++;
+	}
+	if (j <= n) upperBound += (m - weightLimit)*p[j] / w[j];	// calculate promising
+
+	return (upperBound > maxProfit);
+}
+
+void BBknapsack(int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {
+	PQ a;	// my own pq
+
+			// root node
+	int b = bound(level, totalProfit, totalWeight, m, w, p, n);	// bound on root
+	a.insert(level, 0, totalProfit, totalWeight, b);	// add root node to pq
+
+	while (!a.empty()) {
+		int lr = 0;
+		a.remove(level, lr, totalProfit, totalWeight, b);	// Popping first element on pq
+		if (level == n) {	// Printing Result
+			std::cout << std::endl;
+			std::cout << "----------------------------------" << std::endl;
+			std::cout << "result : " << std::endl;
+			std::cout << "level : " << level << std::endl;
+			std::cout << "totalProfit : " << totalProfit << std::endl;
+			std::cout << "totalWeight : " << totalWeight << std::endl;
+			std::cout << "bound : " << b << std::endl;
+
+			ofs << std::endl;
+			ofs << "----------------------------------" << std::endl;
+			ofs << "result : " << std::endl;
+			ofs << "level : " << level << std::endl;
+			ofs << "totalProfit : " << totalProfit << std::endl;
+			ofs << "totalWeight : " << totalWeight << std::endl;
+			ofs << "bound : " << b << std::endl;
+			return;
+		}
+
+		std::cout << "remove : " << level << " " << lr << " " << totalProfit << " " << totalWeight << " " << b << std::endl;
+		if (b > maxProfit && level < n) {
+			// left child
+			lr = level*lr + 1;
+			if (totalWeight + w[level + 1] <= m) {
+				b = bound(level + 1, totalProfit + p[level + 1], totalWeight + w[level + 1], m, w, p, n);
+				a.insert(level + 1, lr, totalProfit + p[level + 1], totalWeight + w[level + 1], b);	// inserting left child in queue
+			}
+
+			// right child
+			lr++;
+			if (totalWeight <= m) {
+				b = bound(level + 1, totalProfit, totalWeight, m, w, p, n);
+				a.insert(level + 1, lr, totalProfit, totalWeight, b);	//inserting right child in queue
+			}
+		}
+
+		a.print();	// print elements in queue
+		std::cout << "level : " << level << std::endl;
+	}
+}
+
+int bound(int level, int totalProfit, int totalWeight, int m, int* w, int* p, int n) { // Calculating Upper Bounds
+	int j = level + 1;
+	int upperBound = totalProfit;
+	int weightLimit = totalWeight;
+
+	while (j <= n && weightLimit + w[j] <= m) {	//calculating bounds before the limit of weight
+		weightLimit += w[j];
+		upperBound += p[j];
+		j++;
+	}
+	if (j <= n) upperBound += (m - weightLimit)*p[j] / w[j];	// calculate promising
+
+	return upperBound;
 }
 
 inline void input1() {
@@ -226,125 +381,4 @@ inline void input3() {
 	std::cout << std::endl;
 	ifs1.close();
 	std::cout << std::endl;
-}
-
-void knapsack(int* w, int** val, int n, int m, int* vals) {
-	// The first line of table
-	for (int j = 0; j <= m; j++) // nothing contained
-		val[0][j] = 0;
-
-	// Calculating Values
-	for (int i = 1; i <= n; i++) { // start with 1st object
-		for (int j = 0; j <= w[i] - 1; j++)		// value before the weight
-			val[i][j] = val[i - 1][j];
-
-		for (int j = w[i]; j <= m; j++)		// value after the weight
-			val[i][j] = max(val[i - 1][j - w[i]] + vals[i], val[i - 1][j]);	// finding maximum between combinations
-	}
-
-	// Print Elements
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= m; j++) std::cout << val[i][j] << " ";
-		std::cout << std::endl;
-	}
-}
-
-int max(int a, int b) {
-	if (a>b) return a;
-	else return b;
-}
-
-void BTknapsack(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {
-	if (totalWeight <= m && level == n) { // answer
-		std::cout << "Contained(1) or not(0)" << std::endl;
-		for (int i = 1; i <= n; i++) std::cout << i << " | ";
-		std::cout << std::endl;
-		for (int i = 1; i <= n; i++) std::cout << btval[i] << " | ";
-		std::cout << std::endl;
-	}
-
-	if (totalWeight <= m && totalProfit > maxProfit) maxProfit = totalProfit;	// updating max profit
-
-	if (Promising(btval, level, totalProfit, totalWeight, maxProfit, m, w, p, n)) {
-		// Left Child
-		btval[level + 1] = 1;
-		BTknapsack(btval, level + 1, totalProfit + p[level + 1], totalWeight + w[level + 1], maxProfit, m, w, p, n);
-		// Right Child
-		btval[level + 1] = 0;
-		BTknapsack(btval, level + 1, totalProfit, totalWeight, maxProfit, m, w, p, n);
-	}
-}
-
-bool Promising(int* btval, int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {		// Calculate promising
-	if (totalWeight >= m) return false;
-	int j = level + 1;
-	int upperBound = totalProfit;
-	int weightLimit = totalWeight;
-
-	while (j <= n && weightLimit + w[j] <= m) {	//calculating bounds before the limit of weight
-		weightLimit += w[j];
-		upperBound += p[j];
-		j++;
-	}
-	if (j <= n) upperBound += (m - weightLimit)*p[j] / w[j];	// calculate promising
-
-	return (upperBound > maxProfit);
-}
-
-void BBknapsack(int level, int totalProfit, int totalWeight, int& maxProfit, int m, int* w, int* p, int n) {
-	PQ a;	// my own pq
-
-			// root node
-	int b = bound(level, totalProfit, totalWeight, m, w, p, n);	// bound on root
-	a.insert(level, 0, totalProfit, totalWeight, b);	// add root node to pq
-
-	while (!a.empty()) {
-		int lr = 0;
-		a.remove(level, lr, totalProfit, totalWeight, b);	// Popping first element on pq
-		if (level == n) {	// Printing Result
-			std::cout << std::endl;
-			std::cout << "----------------------------------" << std::endl;
-			std::cout << "result : " << std::endl;
-			std::cout << "level : " << level << std::endl;
-			std::cout << "totalProfit : " << totalProfit << std::endl;
-			std::cout << "totalWeight : " << totalWeight << std::endl;
-			std::cout << "bound : " << b << std::endl;
-			return;
-		}
-
-		std::cout << "remove : " << level << " " << lr << " " << totalProfit << " " << totalWeight << " " << b << std::endl;
-		if (b > maxProfit && level < n) {
-			// left child
-			lr = level*lr + 1;
-			if (totalWeight + w[level + 1] <= m) {
-				b = bound(level + 1, totalProfit + p[level + 1], totalWeight + w[level + 1], m, w, p, n);
-				a.insert(level + 1, lr, totalProfit + p[level + 1], totalWeight + w[level + 1], b);	// inserting left child in queue
-			}
-
-			// right child
-			lr++;
-			if (totalWeight <= m) {
-				b = bound(level + 1, totalProfit, totalWeight, m, w, p, n);
-				a.insert(level + 1, lr, totalProfit, totalWeight, b);	//inserting right child in queue
-			}
-		}
-
-		a.print();	// print elements in queue
-		std::cout << "level : " << level << std::endl;
-	}
-}
-
-int bound(int level, int totalProfit, int totalWeight, int m, int* w, int* p, int n) { // Calculating Upper Bounds
-	int j = level + 1;
-	int upperBound = totalProfit;
-	int weightLimit = totalWeight;
-
-	while (j <= n && weightLimit + w[j] <= m) {	//calculating bounds before the limit of weight
-		weightLimit += w[j];
-		upperBound += p[j];
-		j++;
-	}
-	if (j <= n) upperBound += (m - weightLimit)*p[j] / w[j];	// calculate promising
-
-	return upperBound;
 }
